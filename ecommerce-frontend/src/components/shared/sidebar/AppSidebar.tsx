4670606d0e102e598cec874489e92387be8b89b1
Ahmad Shoukat <merndeveloper786@gmail.com>
2025-12-18 03:30:48 +0500

    AppSidebar Changing ok

--- ecommerce-frontend/src/components/shared/sidebar/AppSidebar.tsx
+++ ecommerce-frontend/src/components/shared/sidebar/AppSidebar.tsx
@@ -40,7 +40,7 @@ export default function AppSidebar() {
                     href={navItem.url}
                     className={cn(
                       buttonVariants({ variant: "ghost" }),
-                      "relative w-full justify-start px-5 py-4 gap-x-2.5 [&_svg]:size-6 [&_svg]:flex-shrink-0 font-medium text-base focus-visible:bg-accent focus-visible:text-accent-foreground",
+                      "relative w-full justify-start px-5 py-4 gap-x-2.5 [&_svg]:size-6 [&_svg]:flex-shrink-0 font-medium text-base focus-visible:bg-accent focus-visible:text-accent-foreground hover:bg-green-200 hover:text-green-500 transition-colors",
                       pathname === navItem.url &&
                         "bg-accent text-accent-foreground after:content-[''] after:absolute after:top-0 after:left-0 after:h-full after:w-1 after:bg-primary after:rounded-r-lg"
                     )}
